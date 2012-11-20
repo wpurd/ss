34c44736effde9dbad4e8c159586ba0a60cdda21
wpurd <wdyyen@gmail.com>
2012-11-20 15:53:24 -0600

    Contact Form

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>My Contact Form</title>
+</head>
+<body>
+    <header class="main">
+        <h1>My Contact Form</h1>
+    </header>
+    <section class="main">
+        [Form code goes here]
+    </section>
+</body>
+</html>
