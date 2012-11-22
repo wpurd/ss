91ed98e6b41c231ce46d06f1e5df1ce919229343
wpurd <wdyyen@gmail.com>
2012-11-22 07:58:29 -0600

    index.php

--- index.php
+++ index.php
@@ -1,61 +1,37 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
-$name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-
-//Validate first
-if(empty($name)||empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-
-$email_from = 'info@srcsync.com';//<== update the email address
-$email_subject = "New Form submission";
-$email_body = "You have received a new message from the user $name.\n".
-    "Here is the message:\n $message".
+    //cc-by maarten eyskens
+    //start editing the settings here
+    $to = "info@srcsync.com"; //your email here
+    $subject = "New Inquiry"; //the subject here
+    //stop editing here
     
-$to = "info@srcsync.com";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: thank-you.html');
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
+    function secure($string = ''){
+        return stripslashes(trim(htmlspecialchars($string)));
+        }
+    if (isset($_POST['name'])){
+    if (empty($_POST['name'])){
+    echo 'enter your name';
    
-?> 
+    }
+    if (empty($_POST['email'])){
+    echo 'enter your email';
+   
+    }
+    if (empty($_POST['message'])){
+     echo 'enter your message';
+     
+    }
+    $name=secure($_POST['name']);
+    $email=secure($_POST['email']);
+    $message=secure($_POST['message']);
+    
+       $subject = "Contact";
+        $body = "Name: $name Email: $email Message: $message";
+ if (mail($to, $subject, $body)) {
+   echo("<p>Message successfully sent!</p>");
+  } else {
+   echo("<p>Message delivery failed...</p>");
+  }
+    }
+        
+?>
